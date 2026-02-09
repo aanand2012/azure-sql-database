9471c878d878e8361d0bf090ecec77601926cee7
Aanand V Dravid <aanand_2012@yahoo.co.in>
2026-02-09 14:57:05 +0530

    Removed 1 file & corrected the others

--- sql/V1.1.1__drop-tables.sql
+++ sql/V1.1.1__drop-tables.sql
@@ -1,6 +1,12 @@
 
+-- DELETE data from Employee table
+DELETE from Employee
+
+-- DELETE data from Department table
+DELETE from Department
+
 -- DROP Department table
-DROP table Department
+DROP table Employee
 
 -- DROP Employee table
-DROP table employee
+DROP table Department
